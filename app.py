5948e3770c9dba774a8412dc38e8c0f32084b76e
Patryk Świat <patrykswiat@gmail.com>
2024-01-28 18:00:38 +0100

    add baic category selection

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
+import json
+
 from flask import Flask, render_template
+            
+class Category:
+    def __init__(self, id, name, subcategories):
+        self.name = name
+        self.subcategories = subcategories
+        self.id = id
+        
+class CategoryManager:
+    def __init__(self):
+        self.categories = []
+        
+    def load_categories(self, json_file):
+        with open(json_file) as f:
+            categories = json.load(f)
+        for i, category in enumerate(categories):
+            self.categories.append(Category(i, category['name'], category['subcategories']))
+            
 
 app = Flask(__name__)
+categories = CategoryManager()
+categories.load_categories('categories.json')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    categories_list = categories.categories
+    return render_template('index.html', categories=categories_list)
 
 
 if __name__ == '__main__':
--- templates.py
+++ templates.py
@@ -2,21 +2,21 @@ QUESTION_PREFIX_LENGTH = 3
 
 SURVEY_TEMPLATE = (
     "You are a robot, that generates survey questions, based on given category, "
-    "subcategories and number of quetions. \n\n"
-    "Category: popculture; Subcategories: movies, book; Number of questions: 5 \n\n"
-    "Questions: \n"
-    "1. What is your favorite movie? \n"
-    "2. What is your favorite book? \n"
-    "3. Who is your favorite movie character? \n"
-    "4. Who is your favorite book writer \n"
-    "5. What is your favorite movie genre? \n\n"
-    "Category: {category}, Subcategories: {subcategories}, Number of questions: {number_of_questions} \n\n"
-    "Questions: \n"
+    "subcategories and number of quetions.\n\n"
+    "Category: popculture; Subcategories: movies, book; Number of questions: 5;\n\n"
+    "Questions:\n"
+    "1. What is your favorite movie?\n"
+    "2. What is your favorite book?\n"
+    "3. Who is your favorite movie character?\n"
+    "4. Who is your favorite book writer\n"
+    "5. What is your favorite movie genre?\n\n"
+    "Category: {category}; Subcategories: {subcategories}; Number of questions: {number_of_questions};\n\n"
+    "Questions:\n"
 )
 
 ANALYSIS_TEMPLATE = (
-    "You are a robot, that performs analysis of filled surveys. \n\n"
-    "survey: \n"
-    "{survey} \n\n"
-    "analysis: \n"
+    "You are a robot, that performs analysis of filled surveys.\n\n"
+    "survey:\n"
+    "{survey}\n\n"
+    "analysis:\n"
 )
