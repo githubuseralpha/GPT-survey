2d8f140d93714b47a98bb22d14ae4bd25b20c6f1
Patryk Świat <patrykswiat@gmail.com>
2024-01-28 18:06:34 +0100

    add subcategory page, basic flow

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
             
 class Category:
     def __init__(self, id, name, subcategories):
@@ -25,10 +25,26 @@ categories.load_categories('categories.json')
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    categories_list = categories.categories
-    return render_template('index.html', categories=categories_list)
+def category():
+    if request.method == 'POST':
+        data = request.form
+        print(data)
+        category_id = int(data['category'])
+        category = categories.categories[category_id]
+        return redirect(url_for('subcategories', cat_id=category_id))
+    elif request.method == 'GET':
+        categories_list = categories.categories
+        return render_template('index.html', categories=categories_list)
 
+
+@app.route('/subcategories/<cat_id>', methods=['GET', 'POST'])
+def subcategories(cat_id):
+    if request.method == 'POST':
+        pass
+    elif request.method == 'GET':
+        category = categories.categories[int(cat_id)]
+        subcategories = category.subcategories
+        return render_template('subcategories.html', subcategories=subcategories)
 
 if __name__ == '__main__':
     app.run(debug=True)
